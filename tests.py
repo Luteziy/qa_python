4f0f109debdd60186fd24102e834d4b45fa1bbea
Anastasia <aak9595@mail.ru>
2024-05-14 11:29:04 +0300

    new_commit 3

--- tests.py
+++ tests.py
@@ -12,7 +12,6 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 2
 
-
     # 2 Добавление книги 40 символов в названии
 
     def test_add_new_book_with_namelen_more_40(self, collector):
@@ -21,7 +20,6 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 0
 
-
     # 3 Можно ли добавить 2 одинаковых книги
     def test_add_new_book_add_two_books_same_name(self, collector):
         collector.add_new_book(horror_books)
@@ -29,11 +27,9 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) != 2
 
-
     # 4. Устанавливаем жанр книги
     def test_set_book_genre(self, collector, ready_books):
         assert collector.get_book_genre(horror_books) == 'Ужасы'
-
 
     # 5. Добавление книги в жанр по имени
     def test_get_books_genre_add_new_book_with_no_genre(self, collector):
@@ -41,13 +37,11 @@ class TestBooksCollector:
 
         assert collector.get_book_genre(horror_books) == ''
 
-
     # 6. Выводим список книг с определённым жанром
     def test_get_books_with_specific_genre(self, collector, ready_books):
         book_genre = 'Ужасы'
 
         assert collector.get_books_with_specific_genre(book_genre) == [horror_books]
-
 
     # 7. Жанр книги не выводится, если его нет в books_genre
     def test_set_book_genre_no_genre_not_in_books_genre(self, collector, ready_books):
@@ -55,12 +49,10 @@ class TestBooksCollector:
 
         assert collector.get_book_genre(book_genre) == None
 
-
     # 8. возвращаем книги, подходящие детям
     def test_get_books_for_children(self, collector, ready_books):
 
         assert collector.get_books_for_children() == [comedy_books, anime_books, fantasy_books]
-
 
     # 9. Книги с возрастным рейтингом отсутствуют в списке книг для детей
 
@@ -71,12 +63,11 @@ class TestBooksCollector:
                                  (comedy_books, True),
                                  (anime_books, True),
                                  (fantasy_books, True)
-                                 ]
+                             ]
                              )
     def test_get_books_for_children_no_adult_genre(self, collector, ready_books, name, result):
         books_for_child = collector.get_books_for_children()
         assert (name in books_for_child) == result
-
 
     # 10. Добавляет книгу в избранное
     def test_add_book_in_favorites_and_list_is_empty(self, collector, ready_books):
@@ -113,4 +104,3 @@ class TestBooksCollector:
             collector.add_book_in_favorites(favority)
 
         assert len(collector.get_list_of_favorites_books()) == 2
-
